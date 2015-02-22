91a388b8f53338e1e32da637a9e16e8ccc1eca53
h4d4 <estudiante@debianJessie.virtual.uniandes.edu.co>
2015-02-21 20:52:07 -0500

    add classpath for android classes generated

--- androidFlows/jifc-java-libraries.sh
+++ androidFlows/jifc-java-libraries.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 rm -fr jif-classes/auctionPayment;
-jifc -d jif-classes -sourcepath jif-src -addsigcp sig-classes -classpath 'java-classes:jif-classes' -nonrobust $@
+jifc -d jif-classes -sourcepath jif-src -addsigcp sig-classes -classpath 'java-classes:jif-classes:sig-classes' -nonrobust $@
 
 #to code jif
